dac718a120916153c46f8d60abfdfcc15e35351d
yinyumeng <yinym3@gmail.com>
2015-03-02 21:21:58 -0800

    update the brick and GLSurfaceView

--- workspace/app/src/main/java/ucsb2015/bricksbreaker/MainActivity.java
+++ workspace/app/src/main/java/ucsb2015/bricksbreaker/MainActivity.java
@@ -1,21 +1,51 @@
 package ucsb2015.bricksbreaker;
 
+import android.app.Activity;
+import android.content.Context;
 import android.graphics.Color;
+import android.graphics.ImageFormat;
+import android.hardware.Camera;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+
+import java.io.IOException;
+import java.util.List;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
-
+    private MyGLSurfaceView mGLView;
+    private Camera camera;
+    private SurfaceView surfaceview;
+    private boolean preview;
+    private static final String TAG = "OpenGLES20Complete";
+    private FrameLayout mFrameLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        mGLView = new MyGLSurfaceView(this);
+        final Window win = getWindow();
+        win.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+       // setContentView(R.layout.activity_main);
+        surfaceview=new SurfaceView(this);
+        surfaceview.getHolder().addCallback(new SurfaceCallback());
+        //RelativeLayout RLayout = (RelativeLayout)findViewById(R.id.RelativeLayout1);
+        mFrameLayout=new FrameLayout(this);
+        mFrameLayout.addView(mGLView);
+        mFrameLayout.addView(surfaceview);
+        //RLayout.addView(mGLView);
+        setContentView(mFrameLayout);
     }
 
 
@@ -39,6 +69,47 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    private final class SurfaceCallback implements SurfaceHolder.Callback{
+
+        public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                                   int height){
+
+
+        }
+        public void surfaceCreated(SurfaceHolder holder){
+            try {
+                camera = Camera.open();
+                WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+                Display display = wm.getDefaultDisplay();
+                Camera.Parameters parameters = camera.getParameters();
+                List<Camera.Size> sizes = parameters.getSupportedPreviewSizes();
+                Camera.Size   size = sizes.get(0);
+                parameters.setPreviewSize(size.width, size.height);
+
+                parameters.setPictureFormat(ImageFormat.JPEG);
+
+                parameters.setPictureSize(size.width, size.height);
+                camera.setDisplayOrientation(90);
+                camera.setParameters(parameters);
+                camera.setPreviewDisplay(surfaceview.getHolder());
+                camera.startPreview();
+                preview = true;
+
+            } catch(IOException e){
+                Log.e(TAG, e.toString());
+            }
+        }
+        public void surfaceDestroyed(SurfaceHolder holder){
+            if(camera!=null)
+                if(preview)
+                    camera.stopPreview();
+            camera.release();
+
+
+        }
     }
 
 }
--- workspace/app/src/main/java/ucsb2015/bricksbreaker/MyGLSurfaceView.java
+++ workspace/app/src/main/java/ucsb2015/bricksbreaker/MyGLSurfaceView.java
@@ -1,7 +1,84 @@
 package ucsb2015.bricksbreaker;
 
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 /**
  * Created by yumengyin on 3/1/15.
  */
-public class MyGLSurfaceView {
+class MyGLSurfaceView extends GLSurfaceView {
+
+    private final MyGLRenderer mRenderer;
+
+    public volatile int randomInt1,randomInt2,randomInt3;
+
+    public MyGLSurfaceView(Context context) {
+        super(context);
+
+
+        // Create an OpenGL ES 2.0 context.
+        setEGLContextClientVersion(2);
+        setZOrderOnTop(true);
+        setEGLConfigChooser(8,8,8,8,16,0);
+        getHolder().setFormat(PixelFormat.RGBA_8888);
+        // Set the Renderer for drawing on the GLSurfaceView
+        mRenderer = new MyGLRenderer(context);
+
+
+        setRenderer(mRenderer);
+
+        // Render the view only when there is a change in the drawing data
+        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+
+
+
+    }
+
+
+
+
+    private final float TOUCH_SCALE_FACTOR = 180.0f / 320;
+    private float mPreviousX;
+    private float mPreviousY;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        // MotionEvent reports input details from the touch screen
+        // and other input controls. In this case, you are only
+        // interested in events where the touch position changed.
+
+        float x = e.getX();
+        float y = e.getY();
+
+        switch (e.getAction()) {
+            case MotionEvent.ACTION_MOVE:
+
+                float dx = x - mPreviousX;
+                float dy = y - mPreviousY;
+
+                // reverse direction of rotation above the mid-line
+                if (y > getHeight() / 2) {
+                    dx = dx * -1 ;
+                }
+
+                // reverse direction of rotation to left of the mid-line
+                if (x < getWidth() / 2) {
+                    dy = dy * -1 ;
+                }
+                System.out.print("width = " + getWidth());
+                System.out.print("height = " + getHeight());
+                mRenderer.mAngle += (dx + dy) * TOUCH_SCALE_FACTOR;  // = 180.0f / 320
+                requestRender();
+        }
+
+        mPreviousX = x;
+        mPreviousY = y;
+        return true;
+    }
 }
