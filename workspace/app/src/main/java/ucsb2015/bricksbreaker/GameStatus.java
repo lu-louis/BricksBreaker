7e64b33bcdd2766ebd87b4180c7f18f29b97ead9
lu-louis <lu.louis.1202@gmail.com>
2015-03-12 11:24:39 -0700

    GameStatus class update
    adding container info

--- workspace/app/src/main/java/ucsb2015/bricksbreaker/GameStatus.java
+++ workspace/app/src/main/java/ucsb2015/bricksbreaker/GameStatus.java
@@ -8,6 +8,7 @@ public class GameStatus {
     int bricks_left;
     int score;
     int level;
+    Coordinate container_size;
 
     //
     int resolution;     // unit : time, update time interval
@@ -18,5 +19,9 @@ public class GameStatus {
         score       = 0;
         level       = 0;
         speed       = 0;
+        container_size = new Coordinate();
+        container_size.x = 0;
+        container_size.y = 0;
+        container_size.z = 0;
     }
 }
